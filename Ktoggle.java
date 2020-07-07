700980987b1c6d62e81198db131a4f9d576f4552
Anurag singh <anuragz.v1@gmail.com>
2020-07-07 16:29:55 +0530

    Create Ktoggle.java

---
+++ Ktoggle.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+//SIQ = Samsung interview question
+public class Ktoggle {   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        //inputting variables
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int mat[][] = new int [n][m];
+        int k = sc.nextInt();
+
+        //inputting the matrix
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+
+        HashMap<String , Integer> map = new HashMap<>();
+
+        //Storing frequency in map
+        for(int i = 0;i<n;i++){
+            String temp = "";
+            for(int j = 0;j<m;j++){
+                temp+=mat[i][j];
+            }
+            //if map already contains that string increase the freq
+            if(map.containsKey(temp)){
+                int oldFreq = map.get(temp);
+                map.put(temp, oldFreq+1);
+            }
+            //if not insert that string and set freq to 1
+            else{
+                map.put(temp , 1);
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for(Map.Entry<String,Integer>entry:map.entrySet()){
+            //counting number of zeros in each row
+            int num_zeros = 0;
+            for(int i = 0 ;i<entry.getKey().length();i++){
+                if(entry.getKey().charAt(i)=='0')num_zeros++;
+            }
+
+            if(num_zeros %k ==0 && num_zeros<=k){
+                ans = Math.max(entry.getValue(), ans);
+            }
+        }
+
+        System.out.println("Maximum Rows that can be 1 are :"+ ans);
+    }
+
+}
